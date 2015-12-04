d034952fad76382522792030c405352bb0931b44
VasiliyG <vasivik@gmail.com>
2015-12-04 13:09:35 +0600

    Fix: add error mess when no token and cancel button error

--- app/controllers/devise/checkga_controller.rb
+++ app/controllers/devise/checkga_controller.rb
@@ -16,7 +16,7 @@ class Devise::CheckgaController < Devise::SessionsController
   def update
     resource = resource_class.find_by_gauth_tmp(params[resource_name]['tmpid'])
 
-    if not resource.nil?
+    if not resource.nil? || params[:cancel].present?
 
       if resource.validate_token(params[resource_name]['gauth_token'].to_i)
         set_flash_message(:notice, :signed_in) if is_navigational_format?
@@ -24,20 +24,20 @@ class Devise::CheckgaController < Devise::SessionsController
         warden.manager._run_callbacks(:after_set_user, resource, warden, {:event => :authentication})
         respond_with resource, :location => after_sign_in_path_for(resource)
 
-        if not resource.class.ga_remembertime.nil? 
+        if not resource.class.ga_remembertime.nil?
           cookies.signed[:gauth] = {
-            :value => resource.email << "," << Time.now.to_i.to_s,
-            :secure => !(Rails.env.test? || Rails.env.development?),
-            :expires => (resource.class.ga_remembertime + 1.days).from_now
+              :value => resource.email << "," << Time.now.to_i.to_s,
+              :secure => !(Rails.env.test? || Rails.env.development?),
+              :expires => (resource.class.ga_remembertime + 1.days).from_now
           }
         end
       else
-        set_flash_message(:error, :error)
-        redirect_to :root
+        set_flash_message(:error, :bad_token)
+        redirect_to action: :show, id: params[resource_name]['tmpid']
       end
 
     else
-      set_flash_message(:error, :error)
+      set_flash_message(:error, params[:cancel].present? ? :canceled : :error)
       redirect_to :root
     end
   end
