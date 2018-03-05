db75dda8b6fce2a108ff76267dcb11093dfe30e4
Alexey Shepelev <al.shepelev@gmail.com>
2018-03-05 11:12:16 +0300

    Removes to_prepare method from callbacks

--- lib/devise_google_authenticatable/rails.rb
+++ lib/devise_google_authenticatable/rails.rb
@@ -1,13 +1,7 @@
 module DeviseGoogleAuthenticator
   class Engine < ::Rails::Engine # :nodoc:
-    if Rails::VERSION::MAJOR >= 5 && Rails::VERSION::MINOR >= 1
-      ActiveSupport::Reloader.to_prepare do
-        DeviseGoogleAuthenticator::Patches.apply
-      end
-    else
-      ActionDispatch::Callbacks.to_prepare do
-        DeviseGoogleAuthenticator::Patches.apply
-      end
+    ActiveSupport::Reloader.to_prepare do
+      DeviseGoogleAuthenticator::Patches.apply
     end
   end
 end
